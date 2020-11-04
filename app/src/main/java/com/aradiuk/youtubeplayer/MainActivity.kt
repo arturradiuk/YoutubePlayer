9f72b597d276f2f19013c2d7922cd2a5114ecb4b
arturradiuk <arturradiuk@gmail.com>
2020-11-04 21:53:24 +0200

    Connection to the YoutubePlayer API

--- app/src/main/java/com/aradiuk/youtubeplayer/MainActivity.kt
+++ app/src/main/java/com/aradiuk/youtubeplayer/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.aradiuk.youtubeplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : YouTubeBaseActivity() {
+    var playButton: Button? = null
+    private var youTubePlayerView: YouTubePlayerView? = null
+    private var onInitializedListener: YouTubePlayer.OnInitializedListener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton = findViewById(R.id.play_button)
+        youTubePlayerView = findViewById(R.id.youtube_view)
+
+        onInitializedListener = object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider,
+                youTubePlayer: YouTubePlayer,
+                b: Boolean
+            ) {
+                youTubePlayer.loadVideo("https://www.youtube.com/watch?v=DWcJFNfaw9c");
+            }
+
+            override fun onInitializationFailure(
+                provider: YouTubePlayer.Provider,
+                youTubeInitializationResult: YouTubeInitializationResult
+            ) {
+            }
+        }
+
+        playButton?.setOnClickListener(View.OnClickListener {
+            youTubePlayerView?.initialize("AIzaSyBNvZD7VPAchV4PsDIfJdS6o3wOmYPSEXg",onInitializedListener)
+        })
+
     }
 }
